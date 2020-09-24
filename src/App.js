06061790bd93e2db6efac9da1d79af185c354f81
Ilya Peshekhonov <ilya@thepeshka.ru>
2020-09-24 18:28:23 +0300

    add something special

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import {Cursor, UserInput} from "./components";
 import commands from "./commands";
 import Typed from "typed.js";
+import profilesEncoded from "./profiles.js";
+
+const profiles = JSON.parse(atob(profilesEncoded));
 
 function App() {
   const [buffer, setBuffer] = useState([]);
   const [text, setText] = useState("");
   const [commandsCount, setCommandsCount] = useState(7);
   const [animationInProgress, setAnimationInProgress] = useState(true);
+  const [profile, setProfile] = useState(profiles["ER28-0653"])
   const scrollRef = useRef();
   const animationLineRef = useRef();
   const typedjsRef = useRef();
@@ -26,8 +30,16 @@ function App() {
     buffer_.push("$ " + text);
     const command = command_parts[0];
     const command_args = command_parts.slice(1);
-    if (Object.keys(commands).includes(command)) {
-      buffer_ = [...buffer_, ...commands[command](...command_args)];
+    if (command === "loadprofile") {
+      if (command_args.length > 0 && Object.keys(profiles).includes(command_args[0])) {
+        setProfile(profiles[command_args[0]]);
+        buffer_ = [];
+        setAnimationInProgress(true);
+      } else {
+        buffer_ = [...buffer_, "Error: Profile not found!"];
+      }
+    } else if (Object.keys(commands).includes(command)) {
+      buffer_ = [...buffer_, ...commands[command](profile, ...command_args)];
     } else {
       if (command === "clear") {
         buffer_ = [];
@@ -40,7 +52,7 @@ function App() {
   };
   const animate = () => {
     const strings = [
-      "loadprofile ER28-0653",
+      `loadprofile ${profile.id}`,
       "name",
       "bday",
       "contacts",
@@ -67,7 +79,7 @@ function App() {
           const command = command_parts[0];
           const command_args = command_parts.slice(1);
           if (Object.keys(commands).includes(command)) {
-            self.buffer = [...self.buffer, ...commands[command](...command_args)];
+            self.buffer = [...self.buffer, ...commands[command](profile, ...command_args)];
           }
           setBuffer(self.buffer);
         },
@@ -83,7 +95,7 @@ function App() {
   });
   useEffect(() => {
     animate();
-  }, []);
+  }, [profile]);
   return (
     <div className="App">
       <div ref={scrollRef} className="scrollArea">
--- src/commands.js
+++ src/commands.js
@@ -2,35 +2,12 @@ import React from 'react';
 import moment from 'moment';
 
 const commands = {
-  "loadprofile": (profileId) => profileId === "ER28-0653" ? ["Done"] : ["Error: Profile not found!"],
-  "name": () => ["Ilya Peshekhonov"],
-  "contacts": () => [
-    <a href={"https://github.com/thepeshka"}>Github</a>,
-    <a href={"tg://resolve?domain=thepeshka"}>Telegram</a>,
-    <a href={"mailto://ilya@thepeshka.ru"}>ilya@thepeshka.ru</a>
-  ],
-  "location": () => ["Russia, Veliky Novgorod"],
-  "position": () => ["Web-developer @ ZenCat"],
-  "bday": () => [`02.06.1997 (${moment("19970602", "YYYYMMDD").fromNow().split(" ")[0]} y.o.)`],
-  "career": () => [
-    "\u00a007/17\u00a0@",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "\u00a0\u00a0\u00a0SPL\u00a0|\u00a0Jun\u00a0QA",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "\u00a007/18\u00a0@",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "\u00a0\u00a0\u00a0SPL\u00a0|\u00a0Jun\u00a0Dev",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "\u00a007/19\u00a0@",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "\u00a0\u00a0\u00a0SPL\u00a0|\u00a0Mid\u00a0Dev",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "\u00a012/18\u00a0@",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "ZenCat\u00a0|\u00a0Mid\u00a0Web-dev",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0|",
-    "\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0..."
-  ]
+  "name": (profile) => [profile.name],
+  "contacts": (profile) => profile.contacts.map(({link, label}, i) => <a href={link} key={i}>{label}</a>),
+  "location": (profile) => [profile.location],
+  "position": (profile) => [profile.position],
+  "bday": ({dob}) => [`${moment(dob, "YYYYMMDD").format("DD.MM.YYYY")} (${moment(dob, "YYYYMMDD").fromNow().split(" ")[0]} y.o.)`],
+  "career": (profile) => profile.career
 };
 commands["help"] = () => ["Command list: " + Object.keys(commands).join(", ")];
 export default commands;
