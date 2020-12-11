663a0ceca848935ba70c5c15cd1eb1417d3e6a5e
Ilya Peshekhonov <ilya@thepeshka.ru>
2020-12-11 22:37:48 +0300

    fix metadata fix icons fix years old calculation

--- src/commands.js
+++ src/commands.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import moment from 'moment';
 
+const getYearsOld = m => moment(moment() - m).year() - 1970;
+
 const commands = {
   "name": (profile) => [profile.name],
   "contacts": (profile) => profile.contacts.map(({link, label}, i) => <a href={link} key={i}>{label}</a>),
   "location": (profile) => [profile.location],
   "position": (profile) => [profile.position],
-  "bday": ({dob}) => [`${moment(dob, "YYYYMMDD").format("DD.MM.YYYY")} (${moment(dob, "YYYYMMDD").fromNow().split(" ")[0]} y.o.)`],
+  "bday": ({dob}) => [`${moment(dob, "YYYYMMDD").format("DD.MM.YYYY")} (${getYearsOld(moment(dob, "YYYYMMDD"))} y.o.)`],
   "career": (profile) => profile.career
 };
 commands["help"] = () => ["Command list: " + Object.keys(commands).join(", ")];
